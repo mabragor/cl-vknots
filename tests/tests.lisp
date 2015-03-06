3a3ab98cc0f9f3b430232c734630360ca16eb99b
Alexandr Popolitov <popolit@gmail.com>
2015-03-06 18:00:30 +0100

    Add torus(3,3) test

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -7,33 +7,41 @@
 (in-suite cl-vknots)
 
 (test simple-loops
-  (is (equal '((* (** (q "N") 1) "1")) (decompose (deserialize-qed '((1))))))
-  (is (equal '((* (** (q "N") 2) "1")) (decompose (deserialize-qed '((1) (2))))))
-  (is (equal '((* (** (q "N") 3) "1")) (decompose (deserialize-qed '((1) (2) (3)))))))
+  (is (equal '(* (** (q "N") 1) "1") (decompose (deserialize-qed '((1))))))
+  (is (equal '(* (** (q "N") 2) "1") (decompose (deserialize-qed '((1) (2))))))
+  (is (equal '(* (** (q "N") 3) "1") (decompose (deserialize-qed '((1) (2) (3)))))))
       
 (test simple-lines
-  (is (equal '((* (Q "N-1") (* (** (Q "N") 1) "1"))) (decompose (deserialize-qed '((1 1) (2 1))))))
-  (is (equal '((* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+  (is (equal '(* (Q "N-1") (* (** (Q "N") 1) "1")) (decompose (deserialize-qed '((1 1) (2 1))))))
+  (is (equal '(* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1")))
 	     (decompose (deserialize-qed '((1 1) (2 1 2) (3 2))))))
-  (is (equal '((* (Q "N-1") (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1")))))
+  (is (equal '(* (Q "N-1") (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
 	     (decompose (deserialize-qed '((1 1) (2 1 2) (3 2 3) (4 3)))))))
 
 (test 2-strand-diagrams
-  (is (equal '((* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+  (is (equal '(* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1")))
 	     (decompose (deserialize-qed '((1 1 2) (2 1 2))))))
-  (is (equal '((* (Q "2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1")))))
+  (is (equal '(* (Q "2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
 	     (decompose (deserialize-qed '((1 1 2 3) (2 1 2 3))))))
-  (is (equal '((* (Q "2") (* (Q "2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))))
+  (is (equal '(* (Q "2") (* (Q "2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1")))))
 	     (decompose (deserialize-qed '((1 1 2 3 4) (2 1 2 3 4)))))))
 
 (test torus
-  (is (equal '((+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
-		(* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1")))))
+  (is (equal '(+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+		(* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
 	     (decompose (deserialize-qed (torus-dessin 2 3)))))
-  (is (equal '((+ (* (Q "N-2") (+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+  (is (equal '(+ (* (Q "N-2") (+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
 				  (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1")))))
-		(* (Q "N-1") (* (Q "2") (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1")))))))
-	     (decompose (deserialize-qed (torus-dessin 2 4))))))
+		(* (Q "N-1") (* (Q "2") (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))))
+	     (decompose (deserialize-qed (torus-dessin 2 4)))))
+  (is (equal '(+ (* (Q "2") (* (Q "2") (+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+					  (* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))))
+	       (+ (* (Q "N-2") (* (Q "2") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+		(* (Q "N-1") (* (Q "N-1") (* (** (Q "N") 1) "1"))))
+	       (- (* (Q "2") (+ (* (Q "N-2") (* (Q "N-1") (* (** (Q "N") 1) "1")))
+				(* (** (Q "N") 1) (* (Q "N-1") (* (** (Q "N") 1) "1")))))))
+	     (decompose (deserialize-qed (torus-dessin 3 3))))))
+
 
 
 (test deserialize-serialize-loops
