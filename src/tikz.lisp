b3e6309ade832dcb8cfaf780c817a29bd0089f88
Alexandr Popolitov <popolit@gmail.com>
2015-04-15 18:56:30 +0200

    Towards tikz pictures

--- src/tikz.lisp
+++ src/tikz.lisp
@@ -1,7 +1,19 @@
 
 (in-package #:cl-vknots)
 
+(defmacro define-tikz-snippet (name (&rest args) &body body)
+  nil)
 
+(define-tikz-snippet 4-valent-int (t!-x t!-y &key (color :black) (size :normal))
+  ...)
+
+(define-tikz-snippet 3-4-valent-vertices (t!-x t!-y)
+  (join "~%"
+	(4-valent-int t!-x t!-y)
+	(4-valent-int #?"$(t!-x) + 1" t!-y :color :white)
+	(4-valent-int #?"$(t!-x) + 2" t!-y :size :small)))
+    
+    
 (defun tikz-frob ()
   (with-open-file (stream "/tmp/tex.tex" :direction :output :if-exists :supersede)
     (format stream (gen-tex-header))
@@ -9,4 +21,11 @@
     (format stream (gen-tex-tailer)))
   (script1 "pdflatex /tmp/tex.tex")
   (script1 "evince tex.pdf"))
+
+;; Ok, how do I implement convenient interface to TiKZ pictures, that depend on each other,
+;; such that all dependencies are automatically inserted into tex file?
   
+;; The features the DEFINE-TIKZ-SNIPPET seem to be the following:
+;;   * T!-variables define the ones, that are TeX-ified -- they actually become arguments of
+;;     TeX macros.
+;;   * The body should yield string
