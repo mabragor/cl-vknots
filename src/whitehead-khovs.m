5a9195ef050fd4550c475732b5a5addabef140bd
Aleksandr Popolitov <popolit@gmail.com>
2019-07-11 16:04:04 +0200

    Rig seems to be working

--- src/whitehead-khovs.m
+++ src/whitehead-khovs.m
@@ -17,27 +17,19 @@ PyGetWhiteheadizedPD[knot_, aWind_, bWind_] :=
     (* ###    `knot`  -- a knot in any form that can be fed into `PD` of the knot theory ### *)
     (* ###    `aWind` -- number of windings of the a-braid of the double-braid duo ### *)
     (* ###    `bWind` -- number of windings of the b-braid of the double-braid duo ### *)
-    Module[{code, fdWrite},
+    Module[{code, fdWrite, pd},
            (* ### vv dump planar diagram into a file ### *)
            fdWrite = OpenWrite[CCCPythonDir <> CCCInputFname];
-           WriteString[fdWrite, ToString[PD[knot], InputForm]];
+           pd = PD[knot];
+           WriteString[fdWrite, ToString[ReplacePart[pd, FirstPosition[pd, 1] -> 0], InputForm]];
            Close[fdWrite];
            (* ### vv Call a python part of the rig ### *)
-           code = Run["python2 " <> CCCPythonDir <> CCCScriptFname <> " " <> ToString[aWind] <> " " <> ToString[bWind]];
+           code = Run["python2 " <> CCCPythonDir <> CCCScriptFname <> " " <> ToString[aWind] <> " " <> ToString[bWind]
+                      <> " > /dev/null"];
            If[0 =!= code,
               Message[PyGetWhiteheadizedPD::someThingWrongPython],
               (* ### vv Read the whiteheadized diagram from the file ### *)
               Get[CCCPythonDir <> CCCOutputFname]]];
 (* ### ^^ ENDLIB ### *)
 
-PyGetWhiteheadizedPD[Knot[3,1], 2,4]
-
-KnotTheory::loading: Loading precomputed data in PD4Knots`.
-
-StringJoin::string: 
-   String expected at position 2 in 
-    python2 /home/popolit/code/planar-diagrams-py/<>CCCScriptName<> 2 4.
-/usr/bin/python2: can't find '__main__' module in '/home/popolit/code/planar-diagrams-py/'
-
-PyGetWhiteheadizedPD::someThingWrongPython: -- Message text not found --
-
+PyGetWhiteheadizedPD[Knot[3,1], 3,100]
