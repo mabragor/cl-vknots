754f302ed7ded70a111f8c242298a653f4f92728
Aleksandr Popolitov <popolit@gmail.com>
2019-07-23 14:49:38 +0200

    Cleanup a file a little bit

--- src/whitehead-khovs.m
+++ src/whitehead-khovs.m
@@ -46,39 +46,12 @@ KnotToFname[Knot[a_, b_]] :=
     ("rolfsen-knot-" <> ToString[a] <> "-" <> ToString[b]);
 (* ### ^^ ENDLIB ### *)
 
-PrecalculateKhRedWhiteheadizedPDs[Knot[3,1], 0]
+(* ### vv CURWORK ### *)
+CCCRolfsenMults = {1, 1, 2, 3, 7, 21, 49, 165};
+Module[{i, j},
+       For[i = 5, i <= 10, i ++,
+           For[j = 1, j <= CCCRolfsenMults[[i - 2]], j ++,
+               PrecalculateKhRedWhiteheadizedPDs[Knot[i, j], 10]]]];
 
-KnotTheory::loading: Loading precomputed data in PD4Knots`.
-
-Out[3]= /home/popolit/quicklisp/local-projects/cl-vknots/datakh-red-precomp-w\
- 
->    hiteheadized-rolfsen-knot-3-1.m
 
 
-Kh[PyGetWhiteheadizedPD[Knot[3,1], 0, 4] /. {i_Integer :> i + 1}][q, t]
-
-aPD = PyGetWhiteheadizedPD[Knot[3,1], 0, 4]
-
-aPD 
-
-Out[27]= PD[X[12, 5, 13, 6], X[11, 25, 12, 24], X[17, 7, 18, 6], 
- 
->    X[18, 23, 19, 24], X[8, 1, 9, 2], X[7, 29, 8, 28], X[21, 3, 22, 2], 
- 
->    X[22, 27, 23, 28], X[4, 9, 5, 10], X[3, 21, 4, 20], X[25, 11, 26, 10], 
- 
->    X[26, 19, 27, 20], X[16, 29, 17, 30], X[30, 15, 31, 16], 
- 
->    X[14, 31, 15, 32], X[1, 14, 32, 13]]
-
-Out[25]= PD[X[11, 4, 12, 5], X[10, 24, 11, 23], X[16, 6, 17, 5], 
- 
->    X[17, 22, 18, 23], X[7, 0, 8, 1], X[6, 28, 7, 27], X[20, 2, 21, 1], 
- 
->    X[21, 26, 22, 27], X[3, 8, 4, 9], X[2, 20, 3, 19], X[24, 10, 25, 9], 
- 
->    X[25, 18, 26, 19], X[15, 28, 16, 29], X[29, 14, 30, 15], 
- 
->    X[13, 30, 14, 31], X[0, 13, 31, 12]]
-
-KhReduced[PD[Knot[3,1]]][q, t]
